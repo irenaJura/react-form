c68f038450fbdb3e93dd0b0685e740228c3a3701
Irena Jurasek <irenaskydive@gmail.com>
2020-02-07 15:40:22 +0100

    Add constructor and state

--- app/index.js
+++ app/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './style.css';   
 
-export class Input extends React.Component {
+export class Input extends React.Component {  
+  // constructor function takes a parameter of props and calls super(props) on its first line
+  constructor(props) {
+    super(props);
+    // state as with empty string on page load
+    this.state = {
+      userInput: ''
+    }
+  }
+  // listen for a change on the input, create a new method
   render() {
     return (
       <div>
-        <input type="text"/>
+        <input type="text" onChange={this.handleChange} />
         <h1>Hi, I'm H1</h1>
       </div>
     );
